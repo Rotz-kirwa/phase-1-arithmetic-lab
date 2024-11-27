5010e84833bf6f079b4104af86c65fe1ea2ef389
rotzkirwa <eliud.rotich@student.moringaschool.com>
2024-11-27 13:04:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// 1. multiply: an equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // Result: 62
+
+// 2. random: generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+// 3. mod: calculates a remainder that is equal to 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // Remainder is 4
+
+// 4. max: returns 20 as the highest number in the set
+const max = Math.max(5, 10, 15, 20); // Highest number is 20
